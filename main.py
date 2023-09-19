d588c3425fa545cc78d9106d5451067792b48f2c
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2023-09-19 16:27:49 +0300

    View officers and degree name in ui together

--- main.py
+++ main.py
@@ -7,28 +7,35 @@ from sqlalchemy.orm import Session
 
 from mainwindow_ui import Ui_MainWindow
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.degree = None
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
         self.engine = create_engine("sqlite+pysqlite:///database/db_statstrel.db", echo=True)
+        self.select_degree()
         self.load_officers()
-
 
     def load_officers(self):
         with Session(self.engine) as s:
-            # query = """SELECT * FROM officers"""
-            division_id = 2
-            query = ''' 
-            SELECT *
-            FROM officers
-            '''
+            query = """SELECT * FROM officers order by user"""
 
-            rows = s.execute(text(query), {"d_id": division_id})
+            rows = s.execute(text(query))
             for r in rows:
-                self.ui.list_table.addItem(f'{r.user} {r.birthday} {r.degree}')
+                degree_name = self.degree[r.id].degree
+                self.ui.list_table.addItem(f'{r.id}: {r.user} {r.birthday} {degree_name}')
+
+    def select_degree(self):
+        with Session(self.engine) as s:
+            self.degree = {}
+            query = """select * from degree"""
+            rows = s.execute(text(query))
+            for r in rows:
+                self.degree[r.id] = r
+            print(self.degree)
 
     def insert_officer(self):
         with Session(self.engine) as s:
@@ -38,13 +45,11 @@ class MainWindow(QMainWindow):
             '''
             s.execute(text(query), {
                 "user": 'Туполев Алексей Геннадьевич',
-                "birthday":'1992.12.01',
+                "birthday": '1992.12.01',
                 "division": 4,
                 "degree": 3
             })
             s.commit()
-
-
 
 
 if __name__ == '__main__':
