1578f9117174f36cb83d0b71eb39749ace30a9d7
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2024-03-07 14:52:48 +0300

    del old query

--- data/fetch_all_degree.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import text
-from sqlalchemy.orm import Session
-
-
-def fetch_all_degree(s: Session):
-    query = """SELECT * FROM degree"""
-    rows = s.execute(text(query))
-    return rows
--- data/fetch_all_tasks.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import text
-from sqlalchemy.orm import Session
-
-
-def fetch_all_tasks(s: Session):
-    query = """SELECT * FROM task"""
-    rows = s.execute(text(query))
-    return rows
